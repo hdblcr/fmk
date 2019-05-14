3f0d57a72878ffb1b13bf43f97c9b51d4c8ba373
Sarah Peck <sarah.peck@uconn.edu>
2019-05-14 16:48:06 -0400

    Working prototype!

--- functions.js
+++ functions.js
@@ -1,19 +1,46 @@
-function loadList() {
-  return fetch("https://hdblcr.github.io/shipit/SDSAllStars.txt")
-    .then( response => response.text());
+function txt2list(text) {
+  ary = text.split("\r\n");
+  console.log(ary);
+  return ary;
 }
 
-function select () {
-  var characterList = loadList();
-  var total = length(characterList);
+function loadList() {
+  // var text = fetch("https://hdblcr.github.io/shipit/SDSAllStars.txt")
+  //   .then( response => response.text());
+  fetch("./SDSAllStars.txt")
+    // .then( function(response){
+    //   console.log(response.text());
+    //   // doThings(txt2list(response.text()));
+    // })
+    .then(function(response){
+      // console.log("response.text");
+      // console.log(response.text());
+      return response.text();
+      // doThings(txt2list(value));
+    })
+    .then(function(text) {
+      doThings(txt2list(text));
+    });
+}
+
+function select() {
+  loadList();
+}
+
+function doThings(characterList) {
+  var total = characterList.length;
   var character1 = Math.floor(Math.random() * total);
   do {
     var character2 = Math.floor(Math.random() * total);
   } while (character1 === character2);
-  var node = document.getElementById('characters');
-  node.innerHTML('<p>')
-  node.innerHTML(characterList[character1]);
-  node.innerHTML(' and ');
-  node.innerHTML(characterList[character2]);
-  node.innerHTML('!</p>')
+
+  var p = "<p>";
+  var toWrite = p.concat('', characterList[character1], ' and ', characterList[character2], '!</p>');
+
+  document.getElementById("characters").innerHTML = toWrite;
+  // htmlnode.innerHTML('<p>');
+  // htmlnode.innerHTML(characterList[character1]);
+  // htmlnode.innerHTML(' and ');
+  // htmlnode.innerHTML(characterList[character2]);
+  // htmlnode.innerHTML('!</p>');
 }
