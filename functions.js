32f8a4fcd87ccbcdbf040f351f6829e23552174e
Sarah Peck <sarah.peck@uconn.edu>
2019-05-14 22:29:43 -0400

    ERMAGERD! One from each list!

--- functions.js
+++ functions.js
@@ -1,36 +1,63 @@
+var sls = [];
+console.log("ive been called!!");
+loadMe();
+
 function txt2list(text) {
   ary = text.split("\r\n");
-  console.log(ary);
-  return ary;
+  sl = new ShipList(ary);
+  sls.push(sl);
+  // console.log(ary);
+  // return ary;
 }
 
-function loadList() {
+function loadList(file) {
   // var text = fetch("https://hdblcr.github.io/shipit/SDSAllStars.txt")
   //   .then( response => response.text());
-  fetch("./SDSAllStars.txt")
+  fetch(file)
     .then(function(response){
+      console.log("fetch response");
       return response.text();
     })
     .then(function(text) {
-      doThings(txt2list(text));
+      console.log("text");
+      txt2list(text);
     });
 }
 
-function select() {
-  loadList();
+function loadMe() {
+  console.log("loaded??");
+  // load lists
+  loadList("./lists/SDSAllStars.txt");
+  loadList("./lists/characters.txt");
+  loadList("./lists/MCU.txt");
 }
 
-function doThings(list1, list2) {
-  var total1 = list1.length;
-  var c1 = Math.floor(Math.random() * total);
-  var total2 = list2.length;
-  var c1 = Math.floor(Math.random() * total);
-  // do {
-  //   var character2 = Math.floor(Math.random() * total);
-  // } while (character1 === character2);
+function doThings() {
+  // loadMe();
+  var total1 = 1,
+  total2 = 1,
+  c1 = 0,
+  c2 = 0,
+  p = "",
+  toWrite = "";
 
-  var p = "<p><b>";
-  var toWrite = p.concat('', list1[c1], '</b> and <b>', list2[c2], '</b>!</p>');
+  list1 = sls[0];
+  list2 = sls[2];
+
+  total1 = list1.length;
+  c1 = Math.floor(Math.random() * total1);
+  total2 = list2.length;
+  // c2 = Math.floor(Math.random() * total2);
+  do {
+    c2 = Math.floor(Math.random() * total2);
+  } while (list1.d[c1] === list2.d[c2]);
+  console.log(c1);
+  console.log(c2);
+  console.log(list1.d[c1]);
+  console.log(list2.d[c2]);
+
+  p = "<p><b>";
+  toWrite = p.concat('', list1.d[c1], '</b> and <b>', list2.d[c2], '</b>!</p>');
 
   document.getElementById("characters").innerHTML = toWrite;
 }
---
+++ objects.js
@@ -0,0 +1,37 @@
+// class ShipLists {
+//   constructor() {
+//     this = [];
+//   }
+//   add(shipList) {
+//     this.push(shipList);
+//   }
+// }
+
+class ShipList {
+  constructor(arrayFromFile) {
+    this.title = arrayFromFile[0];
+    this.d = arrayFromFile;
+    this.d.shift();
+    this.d.pop();
+    this.rmEmpty();
+    this.length = this.d.length;
+    // shipLists.add(this);
+  }
+  rmEmpty() {
+    var i = 0;
+    for(i=0; i < this.length; i++) {
+      if (this.d[i].length == 0) {
+        this.d.splice(i, i+1);
+      }
+    }
+  }
+  add(name) {
+    this.d.push(name);
+  }
+  adds(names) {
+    var i = 0;
+    for (i=0; i<names.length; i++) {
+      this.add(names[i]);
+    }
+  }
+}
