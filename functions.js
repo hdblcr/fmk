e34208ece34c126fb4cd0b71af06827a5a78e45a
Sarah Peck <sarah.peck@uconn.edu>
2019-08-03 16:19:06 -0400

    create a new page of list

--- functions.js
+++ functions.js
@@ -22,6 +22,27 @@ function loadList(file) {
     });
 }
 
+function fetchList() {
+  var xhttp = new XMLHttpRequest;
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      fetchCharacters();
+    }
+  };
+  xhttp.open("GET", "https://hdblcr.github.io/fmk/", true);
+  xhttp.send();
+}
+
+function fetchCharacters() {
+  loadList("https://hdblcr.github.io/fmk/lists/characters.txt");
+  var doc = document.getElementById("list");
+  for (sl in sls) {
+    var item = document.createElement("li");
+    item.text = sls[sl].title;
+    doc.appendChild(item);
+  }
+}
+
 function fetchOptions() {
   var xhttp = new XMLHttpRequest;
   xhttp.onreadystatechange = function() {
@@ -29,7 +50,7 @@ function fetchOptions() {
       addOptions();
     }
   };
-  xhttp.open("GET", "https://hdblcr.github.io/shipit/", true);
+  xhttp.open("GET", "https://hdblcr.github.io/fmk/", true);
   xhttp.send();
 }
 
@@ -56,42 +77,54 @@ function addOptions() {
 
 function loadMe() {
   loadLists();
-  fetchOptions();
+  // fetchOptions();
   doThings();
 }
 
 function loadLists() {
-  loadList("https://hdblcr.github.io/shipit/lists/SDSAllStars.txt");
-  loadList("https://hdblcr.github.io/shipit/lists/MCU.txt");
-  loadList("https://hdblcr.github.io/shipit/lists/characters.txt");
-  loadList("https://hdblcr.github.io/shipit/lists/realLifeFriends.txt");
+  loadList("https://hdblcr.github.io/fmk/lists/SDSAllStars.txt");
+  loadList("https://hdblcr.github.io/fmk/lists/MCU.txt");
+  loadList("https://hdblcr.github.io/fmk/lists/characters.txt");
+  loadList("https://hdblcr.github.io/fmk/lists/realLifeFriends.txt");
 }
 
 function doThings() {
   var total1 = 1,
   total2 = 1,
+  total3 = 1,
   c1 = 0,
   c2 = 0,
+  c3 = 0,
   p = "",
   toWrite = "",
   l1 = 0,
-  l2 = 1;
+  l2 = 1,
+  l3 = 2;
 
   l1 = document.getElementById("listSelect1").selectedIndex;
   l2 = document.getElementById("listSelect2").selectedIndex;
+  l3 = document.getElementById("listSelect3").selectedIndex;
 
   list1 = sls[l1];
   list2 = sls[l2];
+  list3 = sls[l3];
 
   total1 = list1.length;
   c1 = Math.floor(Math.random() * total1);
   total2 = list2.length;
+  total3 = list3.length;
   do {
     c2 = Math.floor(Math.random() * total2);
   } while (list1.d[c1] == list2.d[c2]);
 
+  do {
+    do {
+      c3 = Math.floor(Math.random() * total3);
+    } while (list1.d[c2] == list2.d[c3]);
+  } while (list1.d[c1] == list2.d[c3]);
+
   p = "<h3>";
-  toWrite = p.concat('', list1.d[c1], ' and ', list2.d[c2], '!</h3>');
+  toWrite = p.concat('', list1.d[c1], ', ', list2.d[c2], ', and ', list3.d[c3], '!</h3>');
 
   document.getElementById("characters").innerHTML = toWrite;
 }
