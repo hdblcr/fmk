28272a26c27a00ab1839b7fa434cef5ecd2c924f
Sarah Peck <sarah.peck@uconn.edu>
2019-08-03 18:10:01 -0400

    bah

--- functions.js
+++ functions.js
@@ -38,10 +38,9 @@ function fetchList() {
 function fetchCharacters() {
   console.log("got to fetchCharacters");
   loadList("https://hdblcr.github.io/fmk/lists/characters.txt");
-  // console.log(sls);
-  // console.log(sls.d);
-  // console.log(sls[0].d);
   var doc = document.getElementById("list");
+  console.log(sls.d);
+  console.log(sls);
   for (sl in sls.d) {
     console.log(sl);
     var item = document.createElement("li");
