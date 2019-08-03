801f1b65e3ae81c99e0960e1d217a400fcaf75c1
Sarah Peck <sarah.peck@uconn.edu>
2019-08-03 17:28:34 -0400

    trying to get the list to display

--- functions.js
+++ functions.js
@@ -23,6 +23,7 @@ function loadList(file) {
 }
 
 function fetchList() {
+  console.log("tesitnnnnnnnnnnnn");
   var xhttp = new XMLHttpRequest;
   xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
